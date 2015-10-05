8da60eef5ce928c3df29e747e009c320738fd2a3
Rafabox001 <rafabox001@gmail.com>
2015-10-04 21:58:15 -0500

    layout design

---
+++ app/src/main/java/com/manguitostudios/primeblend/fragments/CalendarFragment.java
@@ -0,0 +1,28 @@
+package com.manguitostudios.primeblend.fragments;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.manguitostudios.primeblend.R;
+
+/**
+ * Created by Rafael on 04/10/2015.
+ */
+public class CalendarFragment extends Fragment{
+
+    public CalendarFragment() {
+        // Required empty public constructor
+    }
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_calendar, container, false);
+    }
+}
